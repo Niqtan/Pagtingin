213ff71dfa917afe694e4105514d61579403c9ed
Niqtan <niqban123@gmail.com>
2025-02-18 17:12:35 +0800

    multiple interrupts version

--- src/main.c
+++ src/main.c
@@ -27,7 +27,7 @@ uint64_t get_pulse(uint8_t trig_pin, uint8_t echo_pin) {
 
    while (gpio_get(echo_pin) == 0) {
      if (time_us_64() - start_time > TIMEOUT) {
-       printf("Your shit is doomed!\n");
+       printf("You're Cooked!\n");
        return 0;
      }
    }
@@ -36,7 +36,7 @@ uint64_t get_pulse(uint8_t trig_pin, uint8_t echo_pin) {
    
    while (gpio_get(echo_pin) == 1) {
      if (time_us_64() - start_time > TIMEOUT) {
-       printf("Your shit is doomed!\n");
+       printf("You're Cooked!\n");
        return 0;
      }
    }
@@ -54,8 +54,50 @@ uint64_t get_cm(uint8_t trig_pin, uint8_t echo_pin) {
    return (travel_time / 29) / 2;
 }
 
-int wav_position = 0;
+//Slice num for getting the slice num
+uint8_t pwm_slice_num;
 
+
+//Flag for calling PWM
+volatile bool pwm_is_active = false;
+
+void interrupt_initialize() {
+  gpio_set_function(SPEAKER_PIN, GPIO_FUNC_PWM);
+  
+  //Converts our GPIO pin to a slice num
+  pwm_slice_num = pwm_gpio_to_slice_num(SPEAKER_PIN);
+ 
+  //Set the PWM interrupt
+  pwm_clear_irq(pwm_slice_num);
+  pwm_set_irq_enabled(pwm_slice_num, true);
+  
+  //Configuring the Interrupts 
+
+  irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler);
+  irq_set_priority(PWM_IRQ_WRAP, 1);
+  irq_set_enabled(PWM_IRQ_WRAP, true);
+
+  pwm_config le_conf = pwm_get_default_config();
+  pwm_config_set_clkdiv(&le_conf, CLKDIV * 8);
+  //PWM freq = clock freq/(wrap + 1) * clock divider val
+  //So, if we have a 11k sample rate:
+    //11kHz = 125,000,000 Hz / (wrap + 1) * 8.0
+    //To find the wrap val, you may just rearrange the equation via algebra:
+    //wrap = (125,000,000 / 11000 * 8) - 1
+    //wrap = 1419
+  pwm_config_set_wrap(&le_conf, WRAPVAL);   
+
+  pwm_init(pwm_slice_num, &le_conf, true);
+  
+  pwm_set_gpio_level(SPEAKER_PIN, 0);
+
+  while (pwm_is_active) {
+    __wfi();
+  }
+ }
+
+
+int wav_position = 0;
 void pwm_interrupt_handler() {
    //ISR Function for managing the interrupt
    
@@ -72,55 +114,35 @@ void pwm_interrupt_handler() {
 
      /* IN PWM, we have cycles. A cycle is basically something wherein 
      it is a measurement of an on and off time cycle
-
-     In 1 PWM cycle, they decode a sound
      */
+
      pwm_set_gpio_level(SPEAKER_PIN, WAV_DATA[wav_position>>3]); //Divide by 8
      wav_position++;
    }
    else {
      //Reset if it reaches the very end of array
      wav_position = 0;
+     pwm_is_active = false;
    }
 
 }
 
-int pwm_speaker() {
- gpio_set_function(SPEAKER_PIN, GPIO_FUNC_PWM);
+ uint64_t distance;
 
- //Converts our GPIO pin to a slice num
- uint8_t slice_num = pwm_gpio_to_slice_num(SPEAKER_PIN);
-
- //Set the PWM interrupt
- pwm_clear_irq(slice_num);
- pwm_set_irq_enabled(slice_num, true);
-
- //Setting up the interrupt handler
- irq_set_exclusive_handler(PWM_IRQ_WRAP, pwm_interrupt_handler);
- irq_set_enabled(PWM_IRQ_WRAP, true);
-
- //If we set the clock divider to 1, then it has a maximum freq
- // of 125mhz
+ void buzzer_cook_handler() {
+  int buzzer_delay;
   
- pwm_config le_conf = pwm_get_default_config();
- pwm_config_set_clkdiv(&le_conf, CLKDIV);
- //PWM freq = clock freq/(wrap + 1) * clock divider val
- //So, if we want 10kHz output from the rp2040:
-   //20kHz = 125,000,000 Hz / (wrap + 1) * 1.0
-   //To find the wrap val, you may just rearrange the equation via algebra:
-   //wrap = (125,000,000 / 10000) - 1
- pwm_config_set_wrap(&le_conf, WRAPVAL);
- 
- pwm_init(slice_num, &le_conf, true);
+  gpio_acknowledge_irq(BUZZER_PIN, GPIO_IRQ_LEVEL_HIGH);
 
- pwm_set_gpio_level(SPEAKER_PIN, 0);
-
- while (1) {
-  __wfi();
- }
-
- return 0;
+  buzzer_delay = map(distance, 0, THRESHOLD, BUZZER_MIN, BUZZER_MAX);
+      
+  gpio_put(BUZZER_PIN, 1);
+  sleep_ms(buzzer_delay);
+  gpio_put(BUZZER_PIN, 0);
+  sleep_ms(buzzer_delay);
+      
 }
+
 
 int map(int distance, int starting_low, int starting_high, int ending_low, int ending_high) {
   return (distance - starting_low) * (ending_high - ending_low) / (starting_high - starting_low) + ending_low;
@@ -132,34 +154,25 @@ int main() {
  
  setup_pins();
 
-  int buzzer_delay;
-
-  //Flag for calling PWM
-  bool pwm_is_active = false;
+ interrupt_initialize();
 
   while (true) {
-   uint64_t distance = get_cm(TRIG_PIN, ECHO_PIN);
-   
+    distance = get_cm(TRIG_PIN, ECHO_PIN);
+    
     if (distance >= THRESHOLD AND distance <= 50) {
       //Call PWM signal for activating the speaker
-      if (!pwm_is_active) {
-        pwm_speaker();
+      if (pwm_is_active == false) {
+        gpio_put(SPEAKER_PIN, 1);
         pwm_is_active = true;
       }
     }
     else if (distance < THRESHOLD) {
-      buzzer_delay = map(distance, 0, THRESHOLD, BUZZER_MIN, BUZZER_MAX);
     
-      gpio_put(BUZZER_PIN, 1);
-      sleep_ms(buzzer_delay);
-      gpio_put(BUZZER_PIN, 0);
-      sleep_ms(buzzer_delay);
-      
-      pwm_is_active = true;
-   
+      pwm_is_active = false;
     }
    else {
      pwm_is_active = false;
+     gpio_put(SPEAKER_PIN, 0);
      gpio_put(BUZZER_PIN, 0);
    }
 
